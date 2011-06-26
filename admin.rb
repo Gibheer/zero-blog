f3ce935bb6a18fbb4d83e69d9af2755876f710d2
Gibheer <gibheer@gmail.com>
2011-06-26 08:57:59 +0200

    added an admin index and layout

--- admin.rb
+++ admin.rb
@@ -1,7 +1,9 @@
 class Admin < Sinatra::Base
   set $settings
+  enable :sessions
+  set :haml, :layout => :admin_layout
 
   get '/' do
-    'das Adminpanel! und <a href="/">zurueck</a>'
+    haml :admin_index
   end
 end
