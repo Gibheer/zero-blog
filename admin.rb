928be4bf8a8934685fe638e49a3bb1ef0fc20d43
Gibheer <gibheer@gmail.com>
2011-06-30 21:39:01 +0200

    added some changes to the admin layout, stylesheet and added some templates to edit posts

--- admin.rb
+++ admin.rb
@@ -4,18 +4,59 @@ class Admin < Sinatra::Base
   use Rack::Flash, :accessorize => [:error, :warning, :notice]
   set :haml, :layout => :admin_layout
 
-  before do
-    @account = session_read
+  get '/index' do
+    haml :admin_index
   end
 
-  before %r{^(?!\/(login)?$)} do
-    if @account.nil?
-      flash.notice = 'something is wrong'
-      redirect '/admin'
+  get '/post' do
+    @posts = Post.all(:order => [:id.desc])
+    haml :admin_posts
+  end
+
+  get '/post/:id' do
+    @post = Post.get(params[:id])
+    if @post
+      haml :admin_post
     else
-      session[:last_updated] = Time.now
-      flash.notice = 'session is valid!'
+      flash.warning = "Post with id #{params[:id]} not found!"
+      redirect './post'
     end
+  end
+
+  post '/post/:id' do
+    # read the checkbox value
+    if params['post'].has_key?('released')
+      params['post']['released'] = true
+    else
+      params['post']['released'] = false
+    end
+    # get the post and update it
+    @post = Post.get(params[:id])
+    if @post
+      unless @post.update(params[:post])
+        flash.warning = 'Error at saving the post!'
+        flash[:errors] = true
+      end
+      haml :admin_post
+    else
+      flash.warning = "Post with id #{params[:id]} not found!"
+      redirect './post'
+    end
+  end
+
+  get '/post/new' do
+    haml :admin_post_new
+  end
+
+  put '/post' do
+    @post = Post.new(param[:post])
+    if @post.save
+      flash.notice = 'Post saved'
+    else
+      flash.error = 'Error at saving the post'
+      flash[:errors] = @post.errors
+    end
+    haml :admin_post_new
   end
 
   get '/' do
@@ -32,12 +73,13 @@ class Admin < Sinatra::Base
       flash.notice = 'Login successful'
       session[:id] = account.id
       session[:last_updated] = Time.now
-      redirect '/admin/index'
+      # redirect to the url set from the #before block
+      if session.has_key? :to_path
+        redirect "/admin#{session.delete(:to_path)}"
+      else
+        redirect '/admin/index'
+      end
     end
-  end
-
-  get '/index' do
-    haml :admin_index
   end
 
   get '/logout' do
@@ -59,6 +101,28 @@ class Admin < Sinatra::Base
       else
         nil
       end
+    end
+
+    def keys_to_sym hash
+      new_hash = {}
+      hash.each do |k, v|
+        new_hash[k.to_sym] = v
+      end
+      hash = new_hash
+    end
+  end
+
+  before do
+    @account = session_read
+  end
+
+  before %r{^(?!\/(login|stylesheet\.css)?$)} do
+    if @account.nil?
+      flash.warning = 'You are not logged in!'
+      session[:to_path] = request.path_info
+      redirect '/admin'
+    else
+      session[:last_updated] = Time.now
     end
   end
 end
