4e04cce391d66e3f22fd5ce6440ee305e60a6979
Gibheer <gibheer@gmail.com>
2013-10-26 17:52:52 +0200

    add atom feed

--- controller/post.rb
+++ controller/post.rb
@@ -1,15 +1,20 @@
 module Routes
   class Post < Controller
     def self.get(session)
+      define_posts(session)
+      session.options[:render] = 'posts/index'
+    end
+
+    def self.define_posts(session)
       posts = DB[:posts].
         filter(:released => true).
-        select(:posts__id___post_id, :written, :title, :content).
+        select(:posts__id___post_id, :written, :title, :content, :username).
         join(:accounts, :id___account_id => :account_id).
         reverse_order(:written)
-      posts = posts.where(:posts__id => session.options[:id]) if session.options[:id]
-      puts posts.literal(:account__id___account_id)
+      if session.options[:id]
+        posts = posts.where(:posts__id => session.options[:id]) 
+      end
       session.options[:posts]  = posts
-      session.options[:render] = 'posts/index'
     end
   end
 end
